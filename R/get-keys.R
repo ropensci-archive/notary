6dfae26442ed48130e5a87b521006a2b0b1bc092
Jeroen Ooms <jeroenooms@gmail.com>
2017-05-26 17:10:08 -0700

    Removing get-keys

--- R/get-keys.R
+++
@@ -1,5 +0,0 @@
-get_user_key <- function(username){
-  url <- sprintf("https://api.github.com/users/%s/gpg_keys", username)
-  httr::GET(url, add_headers(.headers = build_headers())) -> res
-  httr::content(res, simplify = TRUE, flatten = TRUE)
-}
